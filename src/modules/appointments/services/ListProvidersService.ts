8925c5f3481cd4da09d90e153a46366fa4217a4e
Willian Petri <wopetri11@gmail.com>
2020-08-10 21:20:33 -0300

    Updated ListProvider and ProvidersController

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -31,7 +32,10 @@ class ListProvidersService {
 
       console.log('A query no banco foi feita');
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users)
+      );
     }
 
     return users;
